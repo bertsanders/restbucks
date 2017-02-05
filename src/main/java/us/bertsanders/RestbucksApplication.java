66f7edc953a83d14384b13c9d833d8184b017247
Bert Sanders <thebert99@gmail.com>
2017-02-05 15:21:29 -0600

    output of Spring initializer

---
+++ src/main/java/us/bertsanders/RestbucksApplication.java
@@ -0,0 +1,12 @@
+package us.bertsanders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbucksApplication.class, args);
+	}
+}
