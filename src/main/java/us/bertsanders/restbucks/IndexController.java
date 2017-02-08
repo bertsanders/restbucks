360454429a0905a4bd8e2f274b222b9056250c0a
bertsanders <thebert99@gmail.com>
2017-02-07 22:09:04 -0600

    adding an index

---
+++ src/main/java/us/bertsanders/restbucks/IndexController.java
@@ -0,0 +1,27 @@
+package us.bertsanders.restbucks;
+
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bert on 2/7/17.
+ */
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Resources<String> index()
+    {
+        Resources<String> resources = new Resources<>(new ArrayList<>(),
+                new Link("/order", "order"));
+        return resources;
+    }
+}
